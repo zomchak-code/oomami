a04ed2be2b2c795d71422dd2448b3b3bd0ec87ba
Mykola <mykola@elephantcompany.com>
2026-04-26 12:56:18 +0200

    Fix deploy?

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -6,7 +6,9 @@ const config = {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 	},
-	kit: { adapter: adapter(), }
+	kit: {
+		adapter: adapter({ runtime: 'nodejs22.x' })
+	}
 };
 
 export default config;
